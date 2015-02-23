be14d702b755134aeba3bf1dd60fef3b584df88a
Wei-Chung Wen <morisato3711@gmail.com>
2015-02-23 12:43:20 +0800

    Re-implement type Ethernet.
    Signed-off-by: Wei-Chung Wen <morisato3711@gmail.com>

--- ARP/Ethernet.hs
+++ ARP/Ethernet.hs
@@ -1,47 +1,34 @@
 module ARP.Ethernet where
 
+import ARP.MAC (MAC)
 import Data.Binary (Binary, get, put)
-import Data.Binary.Get (Get, getWord8)
-import Data.Binary.Put (Put, putWord8)
-import Data.Word (Word8)
-import Numeric (readHex)
-import Text.Printf (printf)
+import Data.Binary.Get (Get, getWord16be, getWord32be)
+import Data.Binary.Put (putWord16be, putWord32be)
+import Data.Word (Word16, Word32)
 
-data Ethernet = Ethernet Word8 Word8 Word8 Word8 Word8 Word8
+data Ethernet p = Ethernet
+    { dst       :: MAC
+    , src       :: MAC
+    , etype     :: Word16
+    , payload   :: p
+    , csum      :: Word32
+    }
 
-instance Binary Ethernet where
+instance (Binary p) => Binary (Ethernet p) where
     get = do
-        a <- getWord8
-        b <- getWord8
-        c <- getWord8
-        d <- getWord8
-        e <- getWord8
-        f <- getWord8
-        return (Ethernet a b c d e f)
+        dst     <- get :: Get MAC
+        src     <- get :: Get MAC
+        etype   <- getWord16be
+        payload <- get :: (Binary p) => Get p
+        csum    <- getWord32be
+        return (Ethernet dst src etype payload csum)
 
-    put (Ethernet a b c d e f) = do
-        putWord8 a
-        putWord8 b
-        putWord8 c
-        putWord8 d
-        putWord8 e
-        putWord8 f
+    put (Ethernet dst src etype payload csum) = do
+        put         dst
+        put         src
+        putWord16be etype
+        put         payload
+        putWord32be csum
 
-instance Read Ethernet where
-    readsPrec _ str = [(Ethernet a b c d e f, rest6)]
-        where
-        [(a, ':':rest1)] = readHex str
-        [(b, ':':rest2)] = readHex rest1
-        [(c, ':':rest3)] = readHex rest2
-        [(d, ':':rest4)] = readHex rest3
-        [(e, ':':rest5)] = readHex rest4
-        [(f,     rest6)] = readHex rest5
-
-instance Show Ethernet where
-    show (Ethernet a b c d e f) = printf "%02x:%02x:%02x:%02x:%02x:%02x" a b c d e f
-
-newEthernet :: String -> Ethernet
-newEthernet = read
-
-broadcast :: Ethernet
-broadcast = newEthernet "ff:ff:ff:ff:ff:ff"
+etypeARP :: Word16
+etypeARP = 0x0806
